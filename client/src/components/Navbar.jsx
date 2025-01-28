71e2c0e091883c5f57d874a8e350e1868b554431
Vitor Silva <freirechave.silva@gmail.com>
2025-01-27 21:00:13 -0500

    First HTML page

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav>
+    <h1>YummyPet</h1>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/pet-matching">Pet Matching</a></li>
+      <li><a href="/pet-advice">Pet Advice</a></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
