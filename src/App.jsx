21d2374fdbb578a2b9d6a95857cde5c94a49cd61
Vitor Silva <freirechave.silva@gmail.com>
2025-02-02 22:06:20 -0500

    Manually merged client files from Silva-Branch

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <h1>Welcome to YummyPet!</h1>
+    <p>Your ultimate guide to finding the perfect pet and taking care of them.</p>
+  </div>
+);
+
+export default App;
